04a7b0e0834f929f402440f6ae9eb26b7a1cbfa5
buckaroo69 <diogobento@ua.pt>
2021-06-26 17:36:47 +0100

    profile is now auth-aware

--- src/app/book-editor/book-editor.component.ts
+++ src/app/book-editor/book-editor.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../user.service";
+import {Location} from "@angular/common";
+import {Book} from "../../data/book";
+import {ProfileView} from "../../data/profileView";
 
 @Component({
   selector: 'app-book-editor',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-editor.component.css']
 })
 export class BookEditorComponent implements OnInit {
-
-  constructor() { }
+  bookmarks?: Book[];
+  published?: Book[];
+  constructor(private route:ActivatedRoute,private auth:UserService,private location:Location) { }
 
   ngOnInit(): void {
-  }
+    if (!this.auth.authenticated){this.location.back();}
+    this.auth.profile().subscribe(value => this.loadProfile(value))
+    this.auth.authenticatedChange.subscribe(value => this.onAuthChange(value));
 
+  }
+  private loadProfile(value: ProfileView) {
+    this.bookmarks=value.bookmarks;
+    this.published=value.books;
+
+  }
+  private onAuthChange(value:boolean){
+    if (!value){this.location.back();return;}
+    else{
+      this.auth.profile().subscribe(value => this.loadProfile(value)); //I don't think this will ever happen
+    }
+  }
 }
