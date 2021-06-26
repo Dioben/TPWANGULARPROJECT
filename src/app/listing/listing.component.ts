eeaf0fd27a752f05a961616eeec1b9ff0b822cf5
buckaroo69 <diogobento@ua.pt>
2021-06-26 23:28:25 +0100

    listing page is now self aware

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from "@angular/common";
+import {ActivatedRoute, UrlSegment} from "@angular/router";
+import {ListingService} from "../listing.service";
+
+
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.css']
 })
-export class ListingComponent implements OnInit {
 
-  constructor() { }
+export class ListingComponent implements OnInit {
+  page:number=1;
+  totalpages?:number;
+  type?:string;
+  headerMessage?:string;
+  constructor(private location:Location,private route:ActivatedRoute,private listingService:ListingService) { }
 
   ngOnInit(): void {
+    this.setViewType();
+    this.populatePage();
   }
 
+  private setViewType() {
+    let url:UrlSegment = this.route.snapshot.url.reverse().pop()!;//should only match 1 ever
+    this.type=url.path;
+  }
+
+  private populatePage() {
+    if (this.route.snapshot.paramMap.get('page')){
+      this.page= +this.route.snapshot.paramMap.get('page')!; //defaulted to 1
+    }
+
+  }
 }
