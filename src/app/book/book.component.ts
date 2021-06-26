e70733a01913ac9d86c625c7dbfa8bf73f92b62c
buckaroo69 <diogobento@ua.pt>
2021-06-26 18:38:08 +0100

    load book

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../user.service";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {BookFullView} from "../../data/bookFullView";
+import {BookService} from "../book.service";
+import {Review} from "../../data/review";
 
 @Component({
   selector: 'app-book',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loggedin:boolean; //allow review
+  isauthor:boolean=false; //allow edit
+  book?:BookFullView;
+  bookId?:number;
+  reviews?:Review[];
+  page:number=1;
+  constructor(private auth:UserService,private route:ActivatedRoute,private location:Location,private bookService:BookService) {
+    this.loggedin=auth.authenticated;
+    auth.authenticatedChange.subscribe(value => this.handleLoginChange(value));
   }
 
+  ngOnInit(): void {
+    this.loadBook();
+  }
+
+  private handleLoginChange(value: boolean) {
+    this.loggedin=value;
+    if (this.loggedin){
+      this.isauthor = (this.auth.user_id==this.bookId)
+    }
+  }
+
+  private loadBook() {
+    if (!this.route.snapshot.paramMap.get('pk')){this.location.back();return;} //don't even try if no id
+    this.bookId = +this.route.snapshot.paramMap.get('pk')!;
+    if (this.route.snapshot.paramMap.get('page')){
+      this.page= +this.route.snapshot.paramMap.get('page')!; //defaulted to 1
+    }
+    this.bookService.getBook(this.bookId!).subscribe(value => {this.book=value; if (this.page==1){this.reviews = value.reviews}});
+   if (this.page!=1){
+     this.bookService.getReviews(this.bookId,this.page).subscribe(value => this.reviews=value);
+   }
+  }
 }
