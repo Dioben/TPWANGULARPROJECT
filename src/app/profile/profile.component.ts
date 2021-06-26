8f8ca618210f3292679b17799a7f58ad58223a31
buckaroo69 <diogobento@ua.pt>
2021-06-26 17:40:13 +0100

    moved profile to right class

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from "../../data/book";
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../user.service";
+import {Location} from "@angular/common";
+import {ProfileView} from "../../data/profileView";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  bookmarks?: Book[];
+  published?: Book[];
+  constructor(private route:ActivatedRoute,private auth:UserService,private location:Location) { }
 
   ngOnInit(): void {
+    if (!this.auth.authenticated){this.location.back();}
+    this.auth.profile().subscribe(value => this.loadProfile(value))
+    this.auth.authenticatedChange.subscribe(value => this.onAuthChange(value));
+
+  }
+  private loadProfile(value: ProfileView) {
+    this.bookmarks=value.bookmarks;
+    this.published=value.books;
+
+  }
+  private onAuthChange(value:boolean){
+    if (!value){this.location.back();return;}
+    else{
+      this.auth.profile().subscribe(value => this.loadProfile(value)); //I don't think this will ever happen
+    }
   }
 
 }
