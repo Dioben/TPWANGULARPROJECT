285a9372166346649bc8ee8cb25f33f00f351b8f
buckaroo69 <diogobento@ua.pt>
2021-06-27 12:49:10 +0100

    listing now queries

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import {Location} from "@angular/common";
 import {ActivatedRoute, UrlSegment} from "@angular/router";
 import {ListingService} from "../listing.service";
+import {Book} from "../../data/book";
+import {Chapter} from "../../data/chapter";
+import {query} from "@angular/animations";
+import {ChapterListingView} from "../../data/chapterListingView";
+import {BookListingView} from "../../data/bookListingView";
 
 
 
@@ -14,6 +19,8 @@ import {ListingService} from "../listing.service";
 export class ListingComponent implements OnInit {
   page:number=1;
   totalpages?:number;
+  books?:Book[];
+  chapters?:Chapter[];
   type?:string;
   headerMessage?:string;
   constructor(private location:Location,private route:ActivatedRoute,private listingService:ListingService) { }
@@ -32,6 +39,37 @@ export class ListingComponent implements OnInit {
     if (this.route.snapshot.paramMap.get('page')){
       this.page= +this.route.snapshot.paramMap.get('page')!; //defaulted to 1
     }
+    if (this.type="new"){
+      this.headerMessage= "New Chapters";
+      this.listingService.new(this.page).subscribe(value => this.handleResponseNew(value));
+    }
+    if (this.type="hot"){
+      this.headerMessage= "Popular Books";
+      this.listingService.hot(this.page).subscribe(value => this.handleResponseBooks(value));
+    }
+    if (this.type="top"){
+      this.headerMessage= "Top rated Books";
+      this.listingService.top(this.page).subscribe(value => this.handleResponseBooks(value));
+    }
+    if (this.type="search"){
+      let query:string = this.route.snapshot.queryParams['query'];
+      this.headerMessage="Searching for \""+query+"\"";
+      this.listingService.search(query,this.page).subscribe(value => this.handleResponseBooks(value));
+    }
+  }
 
+  handleResponseNew(value:ChapterListingView){
+    if (value.chapters?.length==0 || this.page>value.pages!){
+      this.headerMessage+="\nNot enough content to populate this page";
+    }
+    this.chapters=value.chapters;
+    this.totalpages=value.pages;
+  }
+  handleResponseBooks(value:BookListingView){
+    if (value.books?.length==0 || this.page>value.pages!){
+      this.headerMessage+="\nNot enough content to populate this page";
+    }
+    this.books=value.books;
+    this.totalpages=value.pages;
   }
 }
