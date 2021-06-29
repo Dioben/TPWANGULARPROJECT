50fb16afecfc81885f2431d4c2c71c6fbf948dab
buckaroo69 <diogobento@ua.pt>
2021-06-29 15:46:10 +0100

    interrupted

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ChapterFullView} from "../../data/chapterFullView";
+import {UserService} from "../user.service";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {ChapterService} from "../chapter.service";
 
 @Component({
   selector: 'app-chapter',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chapter.component.css']
 })
 export class ChapterComponent implements OnInit {
-
-  constructor() { }
+  loggedin:boolean; //allow comment
+  isauthor:boolean=false; //allow edit
+  chapter?:ChapterFullView;
+  chapterId?:number;
+  comments?:Comment[];
+  page:number=1;
+  constructor(private auth:UserService,private route:ActivatedRoute,private location:Location,private chapterService:ChapterService) {
+    this.loggedin=auth.authenticated;
+    auth.authenticatedChange.subscribe(value => this.handleLoginChange(value));
+  }
 
   ngOnInit(): void {
   }
 
+  private handleLoginChange(value: boolean) {
+    this.loggedin=value;
+    if (this.loggedin){
+      this.isauthor = this.auth.user_id==this.bookId
+    }
+  }
 }
