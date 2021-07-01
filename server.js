5bbe2e43e72637b2416015a348342f4a84f23cfe
p-laranjinha <plcasimiro@ua.pt>
2021-07-01 05:46:51 +0100

    Heroku stuff

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/tpw-project"));
+app.use(express.static("./dist/tpwproject"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/tpw-project" });
+  res.sendFile("index.html", { root: "dist/tpwproject" });
 });
 
 app.listen(process.env.PORT || 8080);
