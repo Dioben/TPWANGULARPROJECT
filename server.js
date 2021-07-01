58893ec8ad551bb60a81530d66f51ee1e9f6ded1
p-laranjinha <plcasimiro@ua.pt>
2021-07-01 05:33:44 +0100

    Heroku stuff

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const express = require('express');
 const path = require('path');const app = express();// Serve only the static files form the dist directoryapp.use(express.static(__dirname + '/dist/demo-deploy'));app.get('/*', function(req,res) {
    res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
